b759f96ace36f4ed4f1fd716382c19ed7c446358
bekey <zhaojia0805@163.com>
2018-01-30 10:12:31 +0800

    修改了跨域配置,axios在发送请求时讲话携带cookie,增加一个response过滤器
    修改了跨域配置,axios在发送请求时讲话携带cookie,增加一个response过滤器,修复在request中data为空时会导致的异常

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
+import { Message } from 'element-ui'
 
 // axios 配置
 axios.defaults.timeout = 3000 // 设置超时时间为3s
-
+// 配置axios发送请求时携带cookie
+axios.defaults.withCredentials = true
 // 如果为开发环境，将baseURL设置为服务器地址
 if (process.env.NODE_ENV === 'development') {
   axios.defaults.baseURL = 'http://localhost:8888'
@@ -12,19 +14,43 @@ if (process.env.NODE_ENV === 'development') {
 // 这里处理的是 针对SpringMVC Controller 无法正确获得请求参数的问题
 axios.interceptors.request.use(
   config => {
-    let data = config.data
-    let key = Object.keys(data)
-    // 重写data，由{"name":"name","password":"password"} 改为 name=name&password=password
-    config.data = encodeURI(key.map(name => `${name}=${data[name]}`).join('&'))
-    // 设置Content-Type
     config.headers = {
       'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
     }
+    const data = config.data
+    if (!data) {
+      return config
+    }
+    const key = Object.keys(data)
+    // 重写data，由{"name":"name","password":"password"} 改为 name=name&password=password
+    config.data = encodeURI(key.map(name => `${name}=${data[name]}`).join('&'))
+    // 设置Content-Type
     return config
   },
   error => {
     return Promise.reject(error)
   }
 )
+
+axios.interceptors.response.use(response => {
+  const res = response.data
+  if (res.code !== 200) {
+    Message({
+      message: res.message,
+      type: 'error',
+      duration: 5 * 1000
+    })
+  } else {
+    return response.data
+  }
+}, error => {
+  console.log('err:' + error)
+  Message({
+    message: error.message,
+    type: 'error',
+    duration: 5 * 1000
+  })
+  return Promise.reject(error)
+})
 
 export default axios
